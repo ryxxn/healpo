730e24218e9d7ce1c8bee91297d573775f7169e2
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-30 03:13:50 +0900

    feat: play page

--- src/pages/play/page.tsx
+++ src/pages/play/page.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import { Stopwatch } from './ui/stopwatch';
+import { Counter } from './ui/counter';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
+import { ChevronLeft } from 'lucide-react';
 
 const Page = () => {
   const navigate = useNavigate();
 
+  // mobile 대응
+  const [isBackButtonClick, setIsBackButtonClick] = React.useState(false);
+
+  const onBack = () => {
+    setIsBackButtonClick(true);
+    navigate(-1);
+  };
+
   return (
     <motion.div
+      key="play-page"
       className="max-w-md mx-auto size-full flex flex-col min-h-svh"
-      initial={{ x: '100vw' }}
-      animate={{ x: 0 }}
-      exit={{ x: '-100vw' }}
-      transition={{ duration: 0.5 }}
+      initial={{ x: '100%', opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={isBackButtonClick ? { x: '100%', opacity: 0 } : {}}
+      transition={{ duration: 0.2 }}
     >
-      <header className="sticky top-0 left-0 h-8 p-4 flex">
-        <button onClick={() => navigate(-1)}>뒤로</button>
+      <header className="sticky top-0 left-0 h-16 p-4 flex">
+        <button onClick={onBack}>
+          <ChevronLeft />
+        </button>
       </header>
-      <main className="grow flex justify-center items-center">
-        <Stopwatch />
+      <main className="size-full grow">
+        <Counter />
       </main>
     </motion.div>
   );
