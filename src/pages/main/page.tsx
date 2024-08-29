75041291027e959e404cb7d2a796752c1cd12671
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-30 03:13:40 +0900

    feat: main page

--- src/pages/main/page.tsx
+++ src/pages/main/page.tsx
@@ -1,13 +1,6 @@
-import React from 'react';
 import { Card } from '../../components/card';
-import { ICONS, TABLE } from '../../constants';
-import { useNavigate } from 'react-router-dom';
-import { PATH } from '../../route';
-import { motion } from 'framer-motion';
-import { useIndexedDB } from '../../providers';
-import { uuidv4 } from '../../utils';
-import { IExercise } from '../../types';
 import { ExerciseCard } from './ui';
+import { useAddExercise, useExerciseList } from '../../apis';
 
 const recomendedSet = [
   {
@@ -28,56 +21,23 @@ const recomendedSet = [
 ];
 
 const Page = () => {
-  const navigate = useNavigate();
+  const { data: mySet } = useExerciseList();
 
-  const { addData, getAllData } = useIndexedDB();
+  const addMutation = useAddExercise();
 
-  const [mySet, setMySet] = React.useState<IExercise[]>([]);
-
-  const handleAddData = async () => {
-    const id = uuidv4();
-    const data: IExercise = {
-      id,
-      title: '',
-      iconId: null,
-      sets: [],
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    };
-
-    await addData<IExercise>(TABLE.EXERCISE, data);
-
-    return id;
-  };
-
-  const handleClick = async () => {
+  const handleClick = () => {
     if (window.confirm('추가하시겠습니까?') === false) return;
 
-    const id = await handleAddData();
-    navigate(PATH.detail(id));
+    addMutation.mutate();
   };
 
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const data = await getAllData<IExercise[]>(TABLE.EXERCISE);
-      setMySet(data);
-      console.log(data);
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <motion.div
-      className="max-w-md mx-auto"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <header className="sticky top-0 left-0 h-8 p-4">healpo</header>
-      <main className="container p-4">
-        <article className="mt-8">
+    <div className="max-w-md mx-auto">
+      <header className="sticky top-0 left-0 h-16 p-4 text-primary">
+        healpo
+      </header>
+      <main className="container">
+        <article className="p-4">
           <h1 className="text-3xl font-bold">헬뽀 추천 세트</h1>
           <section className="flex gap-4 overflow-x-auto">
             {recomendedSet.map((set, index) => (
@@ -90,7 +50,7 @@ const Page = () => {
             ))}
           </section>
         </article>
-        <article className="mt-8">
+        <article className="mt-4 p-4">
           <h1 className="text-3xl font-bold">내 운동 세트</h1>
           <section className="flex gap-4 overflow-x-auto">
             <Card
@@ -99,7 +59,7 @@ const Page = () => {
             >
               <p>추가</p>
             </Card>
-            {mySet.map((set, index) => (
+            {mySet?.map((set, index) => (
               <ExerciseCard
                 key={index}
                 id={set.id}
@@ -110,7 +70,7 @@ const Page = () => {
           </section>
         </article>
       </main>
-    </motion.div>
+    </div>
   );
 };
 
