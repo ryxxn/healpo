632d073eb5dc1baba130d62cdff5293b5536bdfb
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-09-04 23:11:34 +0900

    refactor: move play page

--- src/pages/play/ui/counter/countet.util.ts
+++
@@ -1,8 +0,0 @@
-export const formatTime = (time: number): string => {
-  const getSeconds = `0${time % 60}`.slice(-2);
-  const minutes = Math.floor(time / 60);
-  const getMinutes = `0${minutes % 60}`.slice(-2);
-  const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
-
-  return `${getHours}:${getMinutes}:${getSeconds}`;
-};
