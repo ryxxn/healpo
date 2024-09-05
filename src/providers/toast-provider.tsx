5c6754e7f1a8f501a032349f507273a545554178
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-09-05 13:20:31 +0900

    chore: toast icon

--- src/providers/toast-provider.tsx
+++ src/providers/toast-provider.tsx
@@ -1,3 +1,4 @@
+import { CircleCheckBig, CircleX } from 'lucide-react';
 import { Toaster } from 'react-hot-toast';
 
 const ToastProvider = ({ children }: React.PropsWithChildren) => {
@@ -12,7 +13,7 @@ const ToastProvider = ({ children }: React.PropsWithChildren) => {
               'flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800',
             icon: (
               <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
-                {/* <CircleCheckBig className="w-5 h-5" /> */}V
+                <CircleCheckBig className="w-5 h-5" />
               </div>
             ),
           },
@@ -21,11 +22,11 @@ const ToastProvider = ({ children }: React.PropsWithChildren) => {
               'flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800',
             icon: (
               <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
-                {/* <CircleX className="w-5 h-5" /> */}O
+                <CircleX className="w-5 h-5" />
               </div>
             ),
           },
-          duration: 3000,
+          duration: 2000,
         }}
       />
       {children}
